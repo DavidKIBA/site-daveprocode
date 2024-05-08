4c875cfea7acbf6d23f4eaa5f2902f96fd18acd4
Pablo <Ppabocha03@gmail.com>
2024-05-08 21:11:32 +0000

    reglage base de donnée

--- davprocode/settings.py
+++ davprocode/settings.py
@@ -94,16 +94,16 @@ WSGI_APPLICATION = 'davprocode.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
-#     'default': dj_database_url.config()
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
 # }
+
+DATABASES = {
+    'default': dj_database_url.config()
+}
 
 
 # Password validation
