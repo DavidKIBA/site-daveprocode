1244fa6587d8dc3a6995b5d8b4d859d2710acd97
Pablo <Ppabocha03@gmail.com>
2024-05-09 11:38:08 +0000

    update setting

--- davprocode/settings.py
+++ davprocode/settings.py
@@ -24,11 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = config('SECRET_KEY')
-SECRET_KEY = 'django-insecure-67=b_uteu=&ci9dli*9&1n5!@)^p7va%@i&#f^p_xt0p-fw*p&'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['https://daveprocode.com/', 'localhost', '127.0.0.1']
 
