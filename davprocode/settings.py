725edcb3ef67f7b06a72a3656ba24f6f512dcd01
david <davidkiba88@gmail.com>
2024-05-30 02:38:02 +0200

    envoie des images au serveur en ligne

--- davprocode/settings.py
+++ davprocode/settings.py
@@ -151,12 +151,13 @@ USE_TZ = True
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 STATIC_URL = '/static/'
+MEDIA_URL = 'media/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = ['static']
+STATICFILES_DIRS = ['static','media']
 
 
-# MEDIA_URL = 'media/'
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
+#MEDIA_ROOT = os.path.join(BASE_DIR, 'media/') 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
