13a933d6c19687875899cabbaf16811f32c9639b
Pablo <Ppabocha03@gmail.com>
2024-06-10 16:30:25 +0000

    travail sur la page recherche

--- page_app/urls.py
+++ page_app/urls.py
@@ -6,9 +6,6 @@ urlpatterns = [
     path('about/', views.about_page, name='about'),
     path('termes/', views.terme_page, name='termes'),
     path('sitemap/', views.sitemap, name='sitemap'),
-    path('recherche_introuvable/', views.recherche_introuvable, name='recherche_introuvable'),
-    path('recherche/', views.recherche, name='recherche'),
-    # path('bureautique/', views.bureautique, name='bureautique'),
-    # path('formation-infographie/', views.formationInfographie, name='formationInfographie'),
+    path('recherche/', views.search_view, name='recherche'),
 
 ] 
